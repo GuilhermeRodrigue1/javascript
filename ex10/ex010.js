dad212e7463c19d97ebe809edc158ea20fadbf1c
Rodrigues <caosazzy@gmail.com>
2023-12-15 14:46:00 -0300

    oioiooi

---
+++ ex10/ex010.js
@@ -0,0 +1,16 @@
+var vel = 60
+
+console.log(`A velocidade de seu carro é: ${vel}Km/h`)
+
+if (vel > 60) { /*Condição simples visto o if como ´se for´ */
+
+    console.log(`Voce ultrapassou o limite de velocidade permitida`)
+    console.log(`MULTADO!`)
+
+} else { /*Else visto como ´Se não´ */
+
+    console.log(`Voce esta dentro do limite de Velocidade, Parabens!`)
+
+}
+
+console.log(`Dirija sempre usando cinto de segurança!`)
