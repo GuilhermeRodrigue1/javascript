4f3f5a386d88319ff12a255248761815299dd6b4
Rodrigues <caosazzy@gmail.com>
2024-01-04 22:43:05 -0300

    fdsfd

---
+++ ex015/teste.js
@@ -0,0 +1,20 @@
+console.log('Tudo bem')
+
+var contador = 1
+
+do {
+
+console.log(`O numero é: ${contador++}`)
+
+} while (contador <= 9999)
+
+
+/* console.log('Tudo bem')
+
+var contador = 1
+
+while (contador <= 10) {
+
+console.log(`O numero é: ${contador++}`)
+
+} */
