7c7012e4e1d72b7a27d83c226f2dbe9ebefd98e3
Rodrigues <caosazzy@gmail.com>
2024-01-05 18:21:08 -0300

    dasdas

---
+++ ex015/node_f9aa4dec5b52d.tmp.js
@@ -0,0 +1,16 @@
+var c = 0
+
+while (c<=10) {
+
+console.log(`Estrutura while: ${c}`)
+c++
+
+}
+
+
+
+for (var f = 1; f<=10;c++) {
+
+console.log(`Estrutura "For" ${f}`)
+
+}
---
+++ ex015/teste2.js
@@ -0,0 +1,18 @@
+var c = 0
+
+while (c<=10) {
+
+console.log(`Estrutura while: ${c}`)
+c++
+
+}
+
+
+// --------------------------------
+
+
+for (var f = 1; f <= 10 ; f++ ) {
+
+console.log(`Estrutura "For" ${f}`)
+
+}
