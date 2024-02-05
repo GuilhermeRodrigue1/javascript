198629a2400bdfde793cbe2763883eb5545f8b7e
Rodrigues <caosazzy@gmail.com>
2024-02-05 15:07:09 -0300

    att

---
+++ ex017/função01.js
@@ -0,0 +1,16 @@
+function parimpar(n) {
+
+    if (n%2 == 0) {
+
+        return `O numero ${n} é par`
+
+    } else {
+
+        return `O numero ${n} é impar`
+
+    }
+
+}
+
+let testparimp = parimpar(4)
+console.log(testparimp)
